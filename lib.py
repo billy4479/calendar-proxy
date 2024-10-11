95ad0897f9c1c1bc4408f0a9315a29a6ff05d0b7
billy4479 <giachi.ellero@gmail.com>
2024-10-11 09:21:21 +0200

    Simplify get_calendar

--- lib.py
+++ lib.py
@@ -2,30 +2,21 @@ import os
 import requests
 import ics
 
+def get_calendar(cache_path, url):
+    if not os.path.exists(cache_path):
+        cal_text = requests.get(url).text
+        with open(cache_path, "w") as f:
+            f.write(cal_text)
+        return ics.Calendar(cal_text)
+    else:
+        with open(cache_path, "rb") as f:
+            return ics.Calendar(f.read().decode("utf-8"))
 
 def get_calendar_lessons():
-    cache_path = "./cache-lessons.ics"
-    if not os.path.exists(cache_path):
-        cal_text = requests.get(os.getenv("LESSONS_CALENDAR_URL")).text
-        with open(cache_path, "w") as f:
-            f.write(cal_text)
-        return ics.Calendar(cal_text)
-    else:
-        with open(cache_path, "rb") as f:
-            return ics.Calendar(f.read().decode("utf-8"))
-
+    return get_calendar("./cache-lessons.ics", os.getenv("LESSONS_CALENDAR_URL"))
 
 def get_calendar_exams():
-    cache_path = "./cache-exams.ics"
-    if not os.path.exists(cache_path):
-        cal_text = requests.get(os.getenv("EXAMS_CALENDAR_URL")).text
-        with open(cache_path, "w") as f:
-            f.write(cal_text)
-        return ics.Calendar(cal_text)
-    else:
-        with open(cache_path, "rb") as f:
-            return ics.Calendar(f.read().decode("utf-8"))
-
+    return get_calendar("./cache-exams.ics",os.getenv("EXAMS_CALENDAR_URL"))
 
 def make_nice_string(x: str) -> str:
     return (
